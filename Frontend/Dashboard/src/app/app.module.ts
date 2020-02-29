71a70554e75b671fb83470fa9a89deaff4a96ca7
Shubham Mehta <Shubham Mehta>
2020-02-29 09:03:58 +0530

    outer component generate

--- Frontend/Dashboard/src/app/app.module.ts
+++ Frontend/Dashboard/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainLayoutComponent } from './components/main-layout/main-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent
   ],
   imports: [
     BrowserModule,
