07e9426ed3b96c27af5a23de1a23fd8ed17a7c23
Shubham Mehta <Shubham Mehta>
2020-03-06 06:43:51 +0530

    Delete testing code

--- Backend/app.js
+++ Backend/app.js
@@ -1,32 +1,8 @@
 const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
-// const db = require("./util/dbconnection");
+const db = require("./util/dbconnection");
 console.log("hello from app.js");
-// const mysql = require("mysql");
-
-// // First you need to create a connection to the database
-// // Be sure to replace 'user' and 'password' with the correct values
-// const con = mysql.createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password: "",
-//   database: "sitepoint"
-// });
-
-// con.connect(err => {
-//   if (err) {
-//     console.log("Error connecting to Db");
-//     return;
-//   }
-//   console.log("Connection established");
-// });
-// con.query("SELECT * FROM authors", (err, rows) => {
-//   if (err) throw err;
-
-//   console.log("Data received from Db:");
-//   console.log(rows);
-// });
 const userRoutes = require("./routes/auth");
 
 const app = express();
@@ -46,7 +22,6 @@ app.use((req, res, next) => {
   );
   next();
 });
-app.use("/api/user", userRoutes);
 
 app.use((err, req, res, next) => {
   // console.log('error:::,', err);
